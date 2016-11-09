f487e4bf88d8ee7d62b7c028e9a2271291ab8df7
jlittman1995 <jlittman@iastate.edu>
2016-11-08 20:46:06 -0600

    Add files via upload

---
+++ SQLCommands.java
@@ -0,0 +1,38 @@
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.Scanner;
+
+/**
+ * Created by jordanlittman on 11/7/16.
+ */
+public class SQLCommands
+{
+
+    public static void initialize() throws SQLException
+    {
+        try (Connection conn = SimpleDataSource.getConnection())
+        {
+            try (Statement stat = conn.createStatement())
+            {
+
+                Scanner in = new Scanner(System.in);
+                //Create BalanceSheet Table with Assets, Liabilities, StockholdersEquity
+                stat.execute("CREATE TABLE BalanceSheet (Assets VARCHAR(15), Liabilities VARCHAR(15), StockholdersEquity VARCHAR(15))");
+                System.out.print("Enter assets: ");
+                double assets = in.nextDouble();
+                System.out.print("Enter liabilities: ");
+                double liabilities = in.nextDouble();
+                double stockEquity = assets - liabilities;
+                    /*
+                    Sets accounts class variables to user input
+                     */
+                Accounts.totalAssets = assets;
+                Accounts.totalLiabilities = liabilities;
+                Accounts.stockholdersEquity = stockEquity;
+
+            }
+        }
+    }
+}
---
+++ SimpleDataSource.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class SimpleDataSource
+{
+    private static String url;
+    private static String username;
+    private static String password;
+
+    public static void init(String fileName)
+            throws IOException, ClassNotFoundException
+    {
+        Properties properties = new Properties();
+        FileInputStream in = new FileInputStream(fileName);
+        properties.load(in);
+
+        String driver = properties.getProperty("jdbc.driver");
+        url = properties.getProperty("jdbc.url");
+        if (url == null) url = "jdbc:derby:BigJavaDB;create=true";
+        username = properties.getProperty("jdbc.username");
+        if (username == null) username = "";
+        password = properties.getProperty("jdbc.password");
+        if (password == null) password = "";
+        if (driver != null)
+            Class.forName(driver);
+    }
+
+    public static Connection getConnection() throws SQLException
+    {
+        return DriverManager.getConnection(url, username, password);
+    }
+}
